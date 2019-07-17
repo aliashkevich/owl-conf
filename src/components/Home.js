369528bb03ecd2f114bb50b66045fd486e423538
Neli <diakonidzeneli@gmail.com>
2019-07-17 19:17:49 +0200

    remove animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import '../styles/Home.css';
-import Dots from '../microcomponents/dots.js';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Dots />
-        <div className='body-home'>
-          <h1 className='letter'>Home</h1>
-        </div>
-      </React.Fragment>
+      <div className='body-home'>
+        <h1>Home</h1>
+      </div>
     );
   }
 }

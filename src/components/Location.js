8e6b56e4a270ddb9e3c1af33c4d1861aa44a55c7
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-07-17 13:46:33 +0200

    File structure added

---
+++ src/components/Location.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Location extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Location</h1>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/components/Speakers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Speakers extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Speakers</h1>
+      </React.Fragment>
+    );
+  }
+}

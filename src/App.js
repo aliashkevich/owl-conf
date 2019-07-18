9f199dc614a8a1b8743e950dacdb957898e33012
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-07-18 16:12:30 +0200

    added cards + background gradient + moving particles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Speakers from './components/Speakers';
--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import 'tachyons';
+import Particles from 'react-particles-js';
 import './Speakers.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 900,
+      },
+    },
+    style: {
+      color: 'rgb(51, 155, 196)',
+    },
+  },
+};
 
 export default class Speakers extends React.Component {
   render() {
     console.log(this.props.speakers);
     return (
-      <div className='container'>
-        <h1>Speakers</h1>
-        <div className='items'>
-          {this.props.speakers.map(speaker => (
-            <div className='bg-light-green dib br3 pa3 ma2 bw2 shadow-5 grow'>
-              <img className='speakers' src={speaker.image} alt='' />
-              <h2>Name: {speaker.name}</h2>
-              <p>Role: {speaker.role}</p>
-              <p>Topic: {speaker.topic}</p>
-            </div>
-          ))}
+      <div>
+        <Particles className='particles' params={particlesOptions} />
+        <div>
+          <h1 className='title'>Speakers</h1>
+          <div className='container'>
+            {this.props.speakers.map(speaker => (
+              <div className='items bg-gray dib br3 pa3 ma2 bw2 shadow-5 grow'>
+                <div className='item__wrapper'>
+                  <img className='speakersImg grow' src={speaker.image} alt='' />
+                  <h2> {speaker.name}</h2>
+                  <p>
+                    <span>Role:</span> {speaker.role}
+                  </p>
+                  <p>
+                    <span>Topic:</span> {speaker.topic}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );

b3105fa80bea018f071ee01d01b68e6241495672
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-07-18 17:40:46 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
             <Nav />
             <Home />
             <About />
-            <Speakers /> */}
+            <Speakers />
             <Schedule timetable={this.state.timetable} />
             <Address />
             <Jobs />

95061f95a27a80aa0136ac73f86792c9eaf7f73a
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-07-19 12:43:55 +0200

    Removed unused packages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
 import {primary} from '../styles/styles';
